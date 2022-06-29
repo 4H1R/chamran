8b1c7b99349d8500a0a58a0ff076642bfd2c8960
javadSharifi <mohammadjavadsharifi98@gmail.com>
2022-06-29 16:22:06 +0430

    fix bg img

--- resources/ts/Pages/Home.tsx
+++ resources/ts/Pages/Home.tsx
@@ -7,39 +7,47 @@ import Actions from '@/Components/Home/Actions';
 
 function Home() {
   const description =
-    'هنرستانی با بیش از ۵ دهه اموزش و با بهره مندی از معلیمن با تجربه و محیط جذاب به شما در رشد تحصیلی کمک میکند.';
+    'هنرستانی با بیش از ۵ دهه اموزش و با بهره مندی از اساتید با تجربه و محیط جذاب به شما در رشد تحصیلی کمک میکند.';
 
   return (
-    // <div
-    // className="w-screen h-screen"
-    // style={{
-    //   backgroundImage: `url(${asset('img/bg_img.jpg')})`,
-    // }}
-    // >
-    <div className="container flex flex-col items-center h-full space-y-6 lg:flex-row-reverse lg:space-y-0">
-      <Head title="صفحه اصلی" description={description} />
-      <motion.img
-        transition={{ duration: 0.6 }}
-        initial={{ opacity: 0, y: -100 }}
-        animate={{ opacity: 1, y: 0 }}
-        className="animate-bounce-2 h-80 w-full object-cover sm:w-9/12 md:w-7/12 lg:h-auto lg:w-[55%]"
-        src={asset('img/student.png')}
-        alt="Student"
-      />
-      <motion.div
-        transition={{ duration: 0.6 }}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        className="space-y-6 text-center lg:text-right"
+    <>
+      {/* <div
+    className=" h-screen absolute z-10 w-s"
+    style={{
+      backgroundImage: `url(${asset('img/bg_img.jpg')})`,
+    }}
+    >
+      </div> */}
+      <div
+        style={{
+          backgroundImage: `url(${asset('img/bg_img.jpg')})`,
+        }}
+        
+        className="container flex min-h-[90vh] min-w-full flex-col items-center space-y-6 bg-red-200 lg:flex-row-reverse lg:space-y-0"
       >
-        <h1 className="h1">هنرستان شهید چمران</h1>
-        <p className="text-xl leading-loose text-secondary-600">
-          {description}
-        </p>
-        <Actions />
-      </motion.div>
-    </div>
-    // </div>
+        <Head title="صفحه اصلی" description={description} />
+        <motion.img
+          transition={{ duration: 0.6 }}
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="animate-bounce-2 h-80 w-full object-cover sm:w-9/12 md:w-7/12 lg:h-auto lg:w-[55%]"
+          src={asset('img/student.png')}
+          alt="Student"
+        />
+        <motion.div
+          transition={{ duration: 0.6 }}
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="space-y-6 text-center lg:text-right"
+        >
+          <h1 className="h1">هنرستان شهید چمران</h1>
+          <p className="text-xl leading-loose text-secondary-600">
+            {description}
+          </p>
+          <Actions />
+        </motion.div>
+      </div>
+    </>
   );
 }
 
